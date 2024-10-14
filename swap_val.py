d883e624cc649c85ba420e572f0f55ee04c9174f
Rehman Rana <115769841+Rehman79@users.noreply.github.com>
2024-10-14 20:26:03 +0500

    Tenth !!

---
+++ swap_val.py
@@ -0,0 +1,5 @@
+a=int(input("Enter first number : "))
+b=int(input("Enter second number : "))
+print("First Number : ",a,"Second Number : ",b)
+a,b=b,a
+print("First Number : ",a,"Second Number : ",b)
