eef37d149343023134f83aca98649a30d12544a5
Rehman Rana <115769841+Rehman79@users.noreply.github.com>
2024-10-14 20:32:22 +0500

    Tenth enhanced !!

--- swap_val.py
+++ swap_val.py
@@ -3,3 +3,10 @@ b=int(input("Enter second number : "))
 print("First Number : ",a,"Second Number : ",b)
 a,b=b,a
 print("First Number : ",a,"Second Number : ",b)
+
+print("Other approach")
+print("First Number : ",a,"Second Number : ",b)
+a=a+b
+b=a-b
+a=a-b
+print("First Number : ",a,"Second Number : ",b)
